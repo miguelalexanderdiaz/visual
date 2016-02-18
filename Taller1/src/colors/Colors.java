7d0a98ef75aa7c2b826b3e6badbc04a61d5d6a76
Victor Rodriguez <vrodmz@gmail.com>
2016-02-18 08:38:34 -0500

    Estructura del repositorio cambiada para que sea posible poner todos los talleres en el mismo repositorio

--- Taller1/src/colors/Colors.java
+++ Taller1/src/colors/Colors.java
@@ -16,39 +16,44 @@ public class Colors extends PApplet {
 	int color, selectedColor = 0;
 	Boolean freeColor = true;
 
+	@Override
 	public void settings() {
 		size(width, height);
 	}
 
+	@Override
 	public void setup() {
 		overlayer = createGraphics(width, height);
 		background = loadImage("./assets/fractal.jpg");
 		rectMode(CENTER);
 	}
 
+	@Override
 	public void draw() {
 		background(background);
-		//selector
+		// selector
 		color = get(mouseX, mouseY);
 		fill(color);
-		rect(offset * 2 + mouseX, offset + mouseY, bigSquareSize, bigSquareSize);
+		rect((offset * 2) + mouseX, offset + mouseY, bigSquareSize, bigSquareSize);
 		// red component
 		fill(red(color), 0, 0);
-		rect(offset + bigSquareSize + mouseX, offset - smallSquareSize + mouseY, smallSquareSize, smallSquareSize);
+		rect(offset + bigSquareSize + mouseX, (offset - smallSquareSize) + mouseY, smallSquareSize,
+				smallSquareSize);
 		// green component
 		fill(0, green(color), 0);
 		rect(offset + bigSquareSize + mouseX, offset + mouseY, smallSquareSize, smallSquareSize);
 		// blue component
 		fill(0, 0, blue(color));
-		rect(offset + bigSquareSize + mouseX, offset + smallSquareSize + mouseY, smallSquareSize, smallSquareSize);
+		rect(offset + bigSquareSize + mouseX, offset + smallSquareSize + mouseY, smallSquareSize,
+				smallSquareSize);
 
-		//drawing zone
+		// drawing zone
 		overlayer.beginDraw();
 		overlayer.strokeWeight(4);
 		if (mousePressed == true) {
-			if (freeColor){
+			if (freeColor) {
 				selectedColor = color;
-				freeColor=false;
+				freeColor = false;
 			}
 			overlayer.stroke(selectedColor);
 			overlayer.line(mouseX, mouseY, pmouseX, pmouseY);
@@ -57,15 +62,13 @@ public class Colors extends PApplet {
 		image(overlayer, 0, 0);
 	}
 
-
+	@Override
 	public void mouseReleased() {
-		freeColor=true;
+		freeColor = true;
 	}
 
 	public static void main(String args[]) {
 		PApplet.main(new String[] { "--present", "colors.Colors" });
 	}
-	
-
 
 }
