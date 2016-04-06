541559726ef6cdbda6a2c2ccba357ff4996c0ef5
Estudiante <Estudiante>
2016-04-06 10:43:27 -0500

    Avance, mostrando la linea

--- Splines/src/processing/Splines.java
+++ Splines/src/processing/Splines.java
@@ -3,9 +3,13 @@ package processing;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+
 import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.event.MouseEvent;
+import remixlab.dandelion.geom.Vec;
 import remixlab.proscene.InteractiveFrame;
 import remixlab.proscene.Scene;
 
@@ -14,6 +18,7 @@ public class Splines extends PApplet {
 	Scene scene, auxScene;
 	PGraphics canvas, auxCanvas;
 	private List<InteractiveFrame> interactiveFrames;
+	SplineInterpolator sp;
 	private float x;
 	private float y;
 
@@ -24,29 +29,22 @@ public class Splines extends PApplet {
 
 	public void setup() {
 		scene = new Scene(this);
-		scene.setVisualHints(scene.AXES);
+		// scene.setVisualHints(scene.AXES);
+
 		scene.setVisualHints(scene.PICKING);
 		interactiveFrames = new ArrayList<>();
+		addInteractiveFrame(-40, 0);
+		addInteractiveFrame(-30, 40);
 		addInteractiveFrame(0, 0);
-		addInteractiveFrame(10, 0);
-		addInteractiveFrame(20, 0);
-		addInteractiveFrame(30, 0);
+		addInteractiveFrame(30, 40);
 		addInteractiveFrame(40, 0);
+		sp = new SplineInterpolator();
 	}
 
 	public void draw() {
 		background(0);
-
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent event) {
-		if ((mouseButton == RIGHT)) {
-			System.out.println("sss");
-			addInteractiveFrame(0, 0);
-		}
-
-		super.mouseClicked(event);
+		stroke(255);
+		calcSpline();
 	}
 
 	private void addInteractiveFrame(float x, float y) {
@@ -54,6 +52,43 @@ public class Splines extends PApplet {
 		interactiveFrame.setMotionBinding(LEFT, "translate");
 		interactiveFrame.translate(x, y);
 		interactiveFrames.add(interactiveFrame);
+	}
+
+	public void calcSpline() {
+		PolynomialSplineFunction interpolate = null;
+		double x[] = new double[interactiveFrames.size()];
+		double y[] = new double[interactiveFrames.size()];
+		double z[] = new double[interactiveFrames.size()];
+
+		for (int j = 0; j < interactiveFrames.size(); j++) {
+			Vec position = interactiveFrames.get(j).position();
+			x[j] = position.x();
+			y[j] = position.y();
+			z[j] = position.z();
+		}
+
+		try {
+			interpolate = sp.interpolate(x, y);
+		} catch (Exception e) {
+			//e.printStackTrace();
+			return;
+		}
+
+		for (int j = 0; j < x.length - 1; j++) {
+			float x1 = (float) x[j];
+			float z1 = (float) z[j];
+
+			while (x1 < x[j + 1]) {
+				float y1 = (float) interpolate.value(x1);
+				point(x1, y1, z1);
+				drawRectangle(x1, y1, z1);
+				x1 += 0.01;
+			}
+		}
+	}
+
+	private void drawRectangle(float x, float y, float z) {
+		line(x, y, 0, x, y, z);
 	}
 
 	public static void main(String[] args) {
