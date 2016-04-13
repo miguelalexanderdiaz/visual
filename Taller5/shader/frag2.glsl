6b4afb507d6ce0960be01f9879e3e56adc1b1423
Estudiante <Estudiante>
2016-04-13 10:55:59 -0500

    Casi terminado, faltan sliders

--- Taller5/shader/frag2.glsl
+++ Taller5/shader/frag2.glsl
@@ -1,4 +1,5 @@
 uniform float offset;
+uniform float f;
 uniform float A;
 uniform sampler2D texture;
 
@@ -9,6 +10,12 @@ void main()
 {
 	float coordX= vertTexCoord.x;
 	float coordY= vertTexCoord.y;
-	coordX+= A * sin(coordY*10*3.1416+offset)/100;
+	
+	if(coordY > 0.68){
+		coordX+= A * sin(coordY*10*3.1416+offset)/100;
+		coordY+= A * sin(coordX*2*3.1416+offset)/100;
+		coordY = 2 * 0.68 - coordY;
+	}
+	
 	gl_FragColor= texture2D (texture, vec2(coordX, coordY));
 }
--- Taller5/src/processing/Clase.java
+++ Taller5/src/processing/Clase.java
@@ -1,9 +1,8 @@
 package processing;
 
-import controlP5.*;
+import controlP5.ControlP5;
 import processing.core.PApplet;
 import processing.core.PImage;
-import processing.core.PShape;
 import processing.opengl.PShader;
 import remixlab.proscene.Scene;
 
@@ -13,7 +12,7 @@ public class Clase extends PApplet {
 	PImage img;
 	float offset;
 	ControlP5 cp5;
-	private PImage im2;
+	private int size;
 
 	@Override
 	public void settings() {
@@ -24,35 +23,36 @@ public class Clase extends PApplet {
 	public void setup() {
 		scene = new Scene(this);
 		shader = loadShader("shader/frag2.glsl", "shader/vertex1.glsl");
-		img = loadImage("img/w.jpg");
+		img = loadImage("img/w2.jpg");
 		cp5 = new ControlP5(this);
 		cp5.setAutoDraw(false);
-		cp5.addSlider("s").setPosition(20, 20).setSize(200, 20).setRange(0, 100).setValue(1);
-		im2 = loadImage("img/w2.jpg");
+		cp5.addSlider("s").setPosition(20, 20).setSize(200, 20).setRange(0, 10).setValue(1);
+		size = 300;
 	}
 
 	@Override
 	public void draw() {
 		background(0);
+		translate(150, -120);
+		rotateZ(PI / 2);
 
 		beginShape(QUAD);
 		shader(shader);
 		shader.set("offset", offset);
+		shader.set("f", 10);
 		shader.set("A", cp5.getValue("s"));
 		offset += 0.1;
-		
-		
 
 		texture(img);
-		img.resize(100, 100);
+		img.resize(size, size);
 		//// fill(0,0,0);
 		vertex(0, 0, 0, 0);
 		//// fill(255,0,0);
-		vertex(0, 100, 100, 0);
+		vertex(0, size, size, 0);
 		//// fill(0,255,0);
-		vertex(100, 100, 100, 100);
+		vertex(size, size, size, size);
 		//// fill(0,0,255);
-		vertex(100, 0, 0, 100);
+		vertex(size, 0, 0, size);
 		endShape();
 
 		resetShader();
